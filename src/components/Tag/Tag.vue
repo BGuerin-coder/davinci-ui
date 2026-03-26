1a07df6a7b9c9082ca452d696b0085d76411325a
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-03-26 15:54:04 +0100

    feat: add Tag component with variant support for enhanced UI flexibility

---
+++ src/components/Tag/Tag.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="davinci-tag" :class="variant">
+    <slot />
+  </span>
+</template>
+
+<script setup lang="ts">
+import "../../css/tag.css";
+
+export interface TagProps {
+  variant: "brand" | "success" | "error" | "warning" | "neutral";
+}
+
+withDefaults(defineProps<TagProps>(), {
+  variant: "neutral"
+});
+</script>
