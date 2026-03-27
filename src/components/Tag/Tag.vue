b00aab7c3667c9b999a0dc934903a096bcba5351
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-03-27 15:06:06 +0100

    feat: add dismissible functionality to Tag component with icon support

--- src/components/Tag/Tag.vue
+++ src/components/Tag/Tag.vue
@@ -1,21 +1,26 @@
 <template>
-  <span class="davinci-tag" :class="`${variant} ${size} ${appearance}`">
+  <span class="davinci-tag" :class="`${variant} ${size} ${appearance} ${dismissible ? 'davinci-tag--dismissible' : ''}`"
+    @on-click="$emit('dismiss')">
     <slot />
+    <Icon v-if="dismissible" icon="feather:x" class="davinci-tag--dismiss-icon" />
   </span>
 </template>
 
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
 import "../../css/tag.css";
 
 export interface TagProps {
   variant?: "brand" | "success" | "error" | "warning" | "neutral";
   size?: "small" | "medium";
   appearance?: "outline" | "filled";
+  dismissible?: boolean;
 }
 
 withDefaults(defineProps<TagProps>(), {
   variant: "neutral",
   size: "medium",
   appearance: "filled",
+  dismissible: false,
 });
 </script>
