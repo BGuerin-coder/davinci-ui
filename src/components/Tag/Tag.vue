7de2f281d53b972566d6cad952958c9f9098bc0d
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-03-27 15:45:24 +0100

    feat: update Tag component to include icon support and improve layout

--- src/components/Tag/Tag.vue
+++ src/components/Tag/Tag.vue
@@ -1,8 +1,9 @@
 <template>
   <span class="davinci-tag" :class="`${variant} ${size} ${appearance} ${dismissible ? 'davinci-tag--dismissible' : ''}`"
     @on-click="$emit('dismiss')">
-    <slot />
-    <Icon v-if="dismissible" icon="feather:x" class="davinci-tag--dismiss-icon" />
+    <Icon v-if="icon" :icon="`feather:${icon}`" class="davinci-tag--icon" />
+    <span class="davinci-tag--label"><slot /></span>
+    <Icon v-if="dismissible" icon="feather:x" class="davinci-tag--icon davinci-tag--dismiss-icon" />
   </span>
 </template>
 
@@ -15,6 +16,7 @@ export interface TagProps {
   size?: "small" | "medium";
   appearance?: "outline" | "filled";
   dismissible?: boolean;
+  icon?: string;
 }
 
 withDefaults(defineProps<TagProps>(), {
@@ -22,5 +24,6 @@ withDefaults(defineProps<TagProps>(), {
   size: "medium",
   appearance: "filled",
   dismissible: false,
+  icon: '',
 });
 </script>
