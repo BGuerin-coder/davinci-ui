027a20457ef9db7992e9b9bb97b3928341388234
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-03-29 19:07:51 +0200

    feat: design and implement InputTextField component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="davinci-input" :class="`davinci-input--${type}`">
+    <label :for="computedId">{{ label }}</label>
+    <input :type="type" placeholder="Enter text" :id="computedId" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, useId } from 'vue';
+import "../../css/input.css";
+
+export type InputProps = {
+  id?: string;
+  type?: 'text' | 'email' | 'password' | 'number' | 'search' | 'tel' | 'url';
+  label?: string;
+};
+
+const props = withDefaults(defineProps<InputProps>(), {
+  type: 'text',
+  label: ''
+});
+
+const generatedId = useId();
+const computedId = computed(() => props.id ?? generatedId);
+</script>
