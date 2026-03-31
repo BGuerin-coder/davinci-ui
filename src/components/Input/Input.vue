dfc06c29254b3e25cd54034fbc5f103ce0bd5a9d
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-03-31 19:03:26 +0200

    feat: enhance Input component with validation and error handling features

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="davinci-input" :class="`davinci-input--${type}`">
     <label class="davinci-input__label" :for="computedId">{{ label }}</label>
-    <input class="davinci-input__field" :type="type" placeholder="Enter text" :id="computedId" />
+    <input class="davinci-input__field" :class="{
+      'davinci-input__field--invalid': isInvalid
+    }" :type="type" placeholder="Enter text" :id="computedId" v-bind="$attrs" @change="handleInput" />
+    <div v-if="isInvalid || error" class="davinci-input__error-message">
+      <Icon icon="feather:x-circle" class="davinci-input__error-icon" />
+      <span>{{ error }}</span>
+    </div>
+    <div v-else-if="hint" class="davinci-input__hint-message">
+      <Icon icon="feather:info" class="davinci-input__hint-icon" />
+      <span>{{ hint }}</span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, useId } from 'vue';
+import { Icon } from "@iconify/vue";
+import { computed, ref, useId } from 'vue';
 import "../../css/input.css";
+
+defineOptions({ inheritAttrs: false });
 
 export type InputProps = {
   id?: string;
-  type?: 'text' | 'email' | 'password' | 'number' | 'search' | 'tel' | 'url';
+  type?: 'text' | 'email' | 'password' | 'search' | 'tel' | 'url';
+  state?: 'default' | 'error' | 'disabled' | 'success';
   label?: string;
+  hint?: string;
+  error?: string;
 };
 
 const props = withDefaults(defineProps<InputProps>(), {
   type: 'text',
-  label: ''
+  label: '',
+  state: 'default',
+  hint: '',
+  error: '',
 });
+
+const isInvalid = ref(false);
 
 const generatedId = useId();
 const computedId = computed(() => props.id ?? generatedId);
+
+const handleInput = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  isInvalid.value = !input.checkValidity();
+};
 </script>
