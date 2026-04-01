390ae4f94f5778529adba8ed2d57357e37b9c53a
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-01 19:29:10 +0200

    feat: enhance Input component with optional label display and improved state management

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="davinci-input" :class="`davinci-input--${type}`">
-    <label class="davinci-input__label" :for="computedId">{{ label }}</label>
-    <input class="davinci-input__field" :class="{
-      'davinci-input__field--invalid': isInvalid
-    }" :type="type" placeholder="Enter text" :id="computedId" v-bind="$attrs" @change="handleInput" />
+    <label class="davinci-input__label" :for="computedId">
+      {{ label }}
+      <span v-if="hasAttributes($attrs, ['optional'])" class="davinci-input__label--optional">(Optional)</span>
+    </label>
+    <input class="davinci-input__field" :class="addStateClasses($attrs)" :type="type" placeholder="Enter text"
+      :id="computedId" v-bind="$attrs" @change="handleInput" />
     <div v-if="isInvalid || error" class="davinci-input__error-message">
       <Icon icon="feather:x-circle" class="davinci-input__error-icon" />
       <span>{{ error }}</span>
@@ -17,7 +19,7 @@
 
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
-import { computed, ref, useId } from 'vue';
+import { computed, ref, useId, type Attrs } from 'vue';
 import "../../css/input.css";
 
 defineOptions({ inheritAttrs: false });
@@ -25,18 +27,18 @@ defineOptions({ inheritAttrs: false });
 export type InputProps = {
   id?: string;
   type?: 'text' | 'email' | 'password' | 'search' | 'tel' | 'url';
-  state?: 'default' | 'error' | 'disabled' | 'success';
   label?: string;
   hint?: string;
   error?: string;
+  success?: boolean;
 };
 
 const props = withDefaults(defineProps<InputProps>(), {
   type: 'text',
   label: '',
-  state: 'default',
   hint: '',
   error: '',
+  success: false,
 });
 
 const isInvalid = ref(false);
@@ -47,5 +49,18 @@ const computedId = computed(() => props.id ?? generatedId);
 const handleInput = (event: Event) => {
   const input = event.target as HTMLInputElement;
   isInvalid.value = !input.checkValidity();
+};
+
+const addStateClasses = (attrs: Attrs) => {
+  return {
+    'davinci-input__field--invalid': isInvalid.value || props.error,
+    'davinci-input__field--success': props.success,
+    'davinci-input__field--readonly': hasAttributes(attrs, ['readonly']),
+    'davinci-input__field--disabled': hasAttributes(attrs, ['disabled']),
+  };
+};
+
+const hasAttributes = (attrs: Attrs, attributeNames: string[]) => {
+  return attributeNames.some(attr => attr in attrs);
 };
 </script>
