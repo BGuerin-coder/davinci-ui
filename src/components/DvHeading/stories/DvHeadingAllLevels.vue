a6af2d2b097c8fd1026891ccc75c1bd424292f76
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-12 22:22:47 +0200

    feat: add DvHeadingAllLevels story to showcase all heading levels

---
+++ src/components/DvHeading/stories/DvHeadingAllLevels.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="display: flex; flex-direction: column;">
+    <DvHeading as="h1">Heading 1</DvHeading>
+    <DvHeading as="h2">Heading 2</DvHeading>
+    <DvHeading as="h3">Heading 3</DvHeading>
+    <DvHeading as="h4">Heading 4</DvHeading>
+    <DvHeading as="h5">Heading 5</DvHeading>
+    <DvHeading as="h6">Heading 6</DvHeading>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DvHeading from "../DvHeading.vue";
+</script>
