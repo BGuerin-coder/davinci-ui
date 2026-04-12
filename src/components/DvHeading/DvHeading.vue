101433cdf68b9c8fe8e6574d20a4db9e486db357
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-12 22:22:40 +0200

    feat: add DvHeading component with template and props definition

---
+++ src/components/DvHeading/DvHeading.vue
@@ -0,0 +1,15 @@
+<template>
+  <component :is="as" class="dv-heading" :class="`dv-heading--${as}`">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+interface HeadingProps {
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}
+
+const props = withDefaults(defineProps<HeadingProps>(), {
+  as: 'h1',
+});
+</script>
