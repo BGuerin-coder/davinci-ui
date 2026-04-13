c90367dfa4d7b9efe596b2c93062d817b51cafcb
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-13 21:17:35 +0200

    feat: add truncate prop to DvHeading component for text overflow handling

--- src/components/DvHeading/DvHeading.vue
+++ src/components/DvHeading/DvHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="as" class="dv-heading" :class="`dv-heading--${as}`">
+  <component :is="as" class="dv-heading" :class="[`dv-heading--${as}`, { 'dv-heading--truncate': truncate }]">
     <slot />
   </component>
 </template>
@@ -7,9 +7,11 @@
 <script setup lang="ts">
 interface HeadingProps {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  truncate?: boolean;
 }
 
 const props = withDefaults(defineProps<HeadingProps>(), {
   as: 'h1',
+  truncate: false,
 });
 </script>
