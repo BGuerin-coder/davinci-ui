0f2bb5f8f67588d286a0cfca4a2eb2d6f9501b8d
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-14 16:30:04 +0200

    feat: add DvBody component for flexible body text rendering

---
+++ src/components/DvBody/DvBody.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="as" class="dv-body" :class="{ 'dv-body--lead': lead }">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import "../../css/body.css";
+
+interface BodyProps {
+  as?: 'p' | 'div' | 'span';
+  lead?: boolean;
+}
+
+withDefaults(defineProps<BodyProps>(), {
+  as: 'p',
+  lead: false,
+});
+</script>
