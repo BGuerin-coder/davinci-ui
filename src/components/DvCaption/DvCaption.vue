8a790e6fa6cc33a01fc56b8e429f54cc732eb733
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-17 23:10:07 +0200

    feat: implement DvCaption component for flexible caption rendering

---
+++ src/components/DvCaption/DvCaption.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="as" class="dv-caption">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import "../../css/caption.css";
+
+interface CaptionProps {
+  as?: 'span' | 'p' | 'time';
+}
+
+const props = withDefaults(defineProps<CaptionProps>(), {
+  as: 'span',
+});
+</script>
