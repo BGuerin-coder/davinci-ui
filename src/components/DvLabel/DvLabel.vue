4a3e577af66fa7a35018477c5e15348ff8aed95d
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-04-18 15:54:31 +0200

    feat: implement DvLabel component for flexible label rendering

---
+++ src/components/DvLabel/DvLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="as" class="dv-label" :class="{ 'dv-label--uppercase': uppercase }" :for="for">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import "../../css/label.css";
+
+interface LabelProps {
+  as?: 'label' | 'span' | 'div';
+  uppercase?: boolean;
+  for?: string;
+}
+
+withDefaults(defineProps<LabelProps>(), {
+  as: 'label',
+  uppercase: false,
+  for: undefined,
+});
+</script>
