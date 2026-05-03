853c2f1fdf645ae4fff95308d8a0a14aac7b36e8
BGuerin-coder <guerin.benjamin.luigi@gmail.com>
2026-05-03 21:52:11 +0200

    rename folder Button, Input, tag into DvButton, DvInput, DvTag

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./components/DvButton/DvButton.vue";
+export * from "./components/DvBody/DvBody.vue";
+export * from "./components/DvCaption/DvCaption.vue";
+export * from "./components/DvLabel/DvLabel.vue";
+export * from "./components/DvInput/DvInput.vue";
+export * from "./components/DvTag/DvTag.vue";
